8db3b55ce1aea86c0e8dfaa12d836f2e0781fc05
moni patel <patidarmoni41@gmail.com>
2026-08-17 18:20:24 +0530

    Configure ImagineX for Render deployment

--- server/middleware/cloudinaryMiddleware.js
+++ server/middleware/cloudinaryMiddleware.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 // Configuration
 cloudinary.config({
-  cloud_name: 'drporyhxs',
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'drporyhxs',
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
